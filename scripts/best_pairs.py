b2a0bcd62e3a0c5231a8ce74b241232b198667cb
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2016-02-17 15:32:46 +0200

    Generate global list of pairs of words, sorted by score

---
+++ scripts/best_pairs.py
@@ -0,0 +1,42 @@
+from glob import glob
+from operator import itemgetter
+
+
+def main():
+    for outpath in glob("data/output/vocab_*"):
+        with open(outpath + "/pairs.txt") as f:
+            pairs = []
+            for line in f:
+                fields = line.split()
+                if len(fields) == 1:
+                    word1 = fields[0]
+                elif len(fields) == 2:
+                    word2, score = fields
+                    pairs.add((word1, word2, score))
+                else:
+                    raise IOError("Wrong number of fields: " + " ".join(fields))
+        with open(outpath + "/pairs_sorted.txt", "w") as f:
+            f.writelines(",".join(p) + "\n" for p in sorted(
+                pairs, key=itemgetter(2), reverse=True))
+        # My attempt to run the distance utility interactively from Python failed.
+        # Use generate_pairs_file.sh instead and then run this script.
+        # with open(outpath + "/vocab.txt") as f:
+        #     vocab = f.readlines()
+        # for model in glob(outpath + "/*.bin"):
+        #     with subprocess.Popen(["./word2vec/distance", model],
+        #                           stdout=subprocess.PIPE,
+        #                           stdin=subprocess.PIPE) as p:
+        #         pairs = []
+        #         for word in vocab:
+        #             p.stdout.read()
+        #             p.stdin.write(word.encode())
+        #             neighbors = map(str.split, p.stdout.readlines()[2:])
+        #             pairs += [tuple([word] + n) for n in neighbors]
+        #         p.stdin.write("EXIT\n")
+        # with open(outpath + os.path.splitext(model)[0] + "_pairs.csv") as f:
+        #     f.writelines(",".join(p) + "\n" for p in sorted(
+        #         pairs, key=itemgetter(2), reverse=True))
+
+
+if __name__ == "__main__":
+    main()
--- scripts/explore_w2v_param_space.py
+++ scripts/explore_w2v_param_space.py
@@ -2,19 +2,20 @@ import os
 from subprocess import call
 
 # run from root dir
-THREADS=31
+THREADS = 31
 # ignore words repeated under threshold amount of times (0 = don't ignore any words)
-vocabulary_thresholds=[0,5,10]
+vocabulary_thresholds = [0, 5, 10]
 # word2vec prediction windows around each word
 windows = [8, 10]
 # output vector dimension
 size = [152, 300]
 # number of iterations
-iter = [1, 5]
+iterations = [1, 5]
+
 
 def main():
-    for it in iter:
-        print('number of iters: '+str(it))
+    for it in iterations:
+        print('number of iters: ' + str(it))
         for sz in size:
             for win in windows:
                 for voc_th in vocabulary_thresholds:
@@ -22,24 +23,25 @@ def main():
                     if not os.path.exists(outpath):
                         os.makedirs(outpath)
                     cmd = './word2vec/word2vec ' \
-                                '-train data/corpus.txt ' \
-                                '-output data/output/vocab_{vocab_thres}/vectors_window_{window}_size_{size}_iter_{iter}.bin ' \
-                                '-cbow 0 ' \
-                                '-size {size} ' \
-                                '-window {window} ' \
-                                '-negative 25 ' \
-                                '-hs 0 ' \
-                                '-sample 1e-4 ' \
-                                '-threads {threads} ' \
-                                '-binary 1 ' \
-                                '-iter {iter} ' \
-                                '-save-vocab data/output/vocab_{vocab_thres}/vocab.txt ' \
-                                '-min-count {vocab_thres}' \
-                                .format(vocab_thres=voc_th, window=win, size=sz, iter=it, threads=THREADS)
+                          '-train data/corpus.txt ' \
+                          '-output data/output/vocab_{vocab_thres}/vectors_window_{window}_size_{size}_iter_{iter}.bin ' \
+                          '-cbow 0 ' \
+                          '-size {size} ' \
+                          '-window {window} ' \
+                          '-negative 25 ' \
+                          '-hs 0 ' \
+                          '-sample 1e-4 ' \
+                          '-threads {threads} ' \
+                          '-binary 1 ' \
+                          '-iter {iter} ' \
+                          '-save-vocab data/output/vocab_{vocab_thres}/vocab.txt ' \
+                          '-min-count {vocab_thres}' \
+                        .format(vocab_thres=voc_th, window=win, size=sz, iter=it, threads=THREADS)
                     retval = call(cmd.split())
                     if retval != 0:
                         print('An error has occured. Quitting.')
                         break
+
 
 if __name__ == '__main__':
     main()
